d6b2bb613e7e3e02cc9bf50ccbf2f0802e700065
pravin-kavthale <kavthalepravin@gmail.com>
2025-07-18 13:54:14 +0530

    feat: Implement user authentication with login, logout, and profile features

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 from . import views
 from users import views as user_views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path("", views.Home,name="blog-home"),
     path("about/",views.about,name='blog-about'),
     path('contact/', views.contact, name='blog-contact'),
+    path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
+    path('profile/', user_views.profile, name='profile'),
     path("register/",user_views.register,name="user-register"),
     
 ]
--- users/form.py
+++ users/form.py
@@ -2,9 +2,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
+
 class UserRegistrationForm(UserCreationForm):
     email=forms.EmailField()
-    
 
     class Meta:
         model=User
