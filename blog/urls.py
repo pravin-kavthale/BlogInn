15b3404496b27082a90cfc486da21d2c3e9b10d6
pravin-kavthale <kavthalepravin@gmail.com>
2025-07-22 13:23:09 +0530

    feat: Implement CRUD functionality for blog posts with corresponding views and templates

--- blog/urls.py
+++ blog/urls.py
@@ -4,10 +4,21 @@ from . import views
 from users import views as user_views
 from django.contrib.auth import views as auth_views
 
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView)
+
 
 urlpatterns = [
-    path("", views.Home,name="blog-home"),
+    path("", PostListView.as_view(),name="blog-home"),
+    path("post/<int:pk>/",PostDetailView.as_view(),name="post-detail"),
     path("about/",views.about,name='blog-about'),
+    path('post/new/',PostCreateView.as_view(),name='post-create'),
+    path('post/<int:pk>/update/',PostUpdateView.as_view(),name='post-update'),
+    path('post/<int:pk>/delete',PostDeleteView.as_view(),name='post-delete'),
     path('contact/', views.contact, name='blog-contact'),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,54 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 
-def Home(request):
-    posts = Post.objects.all().order_by('-date_posted')  # 👈 Order by newest first
-    return render(request, 'blog/home.html', {'posts': posts})
+class PostListView(ListView):
+    model=Post
+    template_name="blog/home.html"
+    context_object_name = 'posts'
+    ordering=['-date_posted']
+
+class PostDetailView(DetailView):
+    model=Post
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model=Post
+    fields=['title','content']
+
+    def form_valid(self,form):
+        form.instance.author=self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model=Post
+    fields=['title','content']
+    def form_valid(self,form):
+        form.instance.author=self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post=self.get_object()
+        if self.request.user==post.author:
+            return True
+        else:
+            return False
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model=Post
+    success_url="/"
+    def test_func(self):
+        post=self.get_object()
+        if post.author==self.request.user:
+            return True
+        else:
+            return False
 
 def about(request):
     return render(request,'blog/about.html',{"title":"About"})
@@ -12,5 +57,3 @@ def contact(request):
     return render(request,'blog/contact.html',{"title":"Contact"})
 
 
-
-    # Create your views here.
