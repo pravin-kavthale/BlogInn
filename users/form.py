f6ce645d03f66e813a7c2cdf5894359c63b5ae85
pravin-kavthale <kavthalepravin@gmail.com>
2025-07-31 21:36:36 +0530

    feat: Enhance user registration form with email validation and improved styling

--- users/form.py
+++ users/form.py
@@ -2,6 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 from .models import profile
+from django.core.exceptions import ValidationError
 
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -9,6 +10,12 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+    
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("This email is already in use.")
+        return email
 
 
 class UserUpdateForm(forms.ModelForm):
